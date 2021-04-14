afc49884505804f4a53014452533614c3472d706
babs20 <thevillemc@gmail.com>
2021-04-14 17:47:23 -0400

    create first story and component

---
+++ src/components/Task.stories.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-anonymous-default-export */
+import Task from './Task';
+
+// Export Component to Storybook
+export default {
+  component: Task,
+  title: 'Task',
+};
+
+// Create a template for creating stories for each component state
+const Template = args => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
